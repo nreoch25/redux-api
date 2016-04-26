9c1fb3660fa3c53dfe85f0085967c1a1ae023487
Nigel Reoch <nigel.reoch@cbc.ca>
2016-04-26 17:31:48 -0400

    Added authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+  //User already has been authorized by email and password
+  //We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
